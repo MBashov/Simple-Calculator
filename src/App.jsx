4d30dd74077021b3b5e2df5c484119a03ac9ed99
MBashov <galacticos.mb@gmail.com>
2025-03-03 15:23:42 +0100

    Add functions

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,49 @@ function App() {
 
         if (
             (buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/') &&
-            (value === '' || /[+\-*\/]$/.test(value)) // Prevent multiple consecutive operators
+            (value === '' || /[+\-*\/]$/.test(value))
         ) {
-            return; // Do nothing if it's an invalid operator press
+            return;
         }
 
         setValue(value + buttonValue);
     }
 
+    const handleClear = () => {
+        setValue('');
+        setError(false);
+    }
 
+    const handleDelete = () => {
+        setValue(value.slice(0, -1));
+        setError(false);
+    };
+
+    const handleEquals = () => {
+
+        try {
+            if (value === '' || /[+\-*\/]$/.test(value)) {
+                throw new Error('Invalid Expression');
+            }
+
+            // Handle division by zero
+            if (value.includes('/0')) {
+                throw new Error('Cannot divide by zero');
+            }
+
+            const result = eval(value);
+
+            // If the result is a valid number, set the value
+            if (!isNaN(result)) {
+                setValue(result.toString());
+            } else {
+                throw new Error('Invalid Expression');
+            }
+        } catch (err) {
+            setError(true);
+            setValue('Error'); // Display error message
+        }
+    };
 
     return (
         <>
@@ -31,33 +65,34 @@ function App() {
                         <div>
                             <input type="text" readOnly value={value} />
                         </div>
+                        {error && <div className="error-message">Invalid Expression</div>}
                         <div>
-                            <input type="button" value="AC" onClick={() => setValue('')} />
-                            <input type="button" value="DE" onClick={() => setValue(value.slice(0, -1))} />
-                            <input type="button" value="/" onClick={(e) => setValue(value + e.target.value)} />
+                            <input type="button" value="AC" onClick={handleClear} />
+                            <input type="button" value="DE" onClick={handleDelete} />
+                            <input type="button" value="/" onClick={handleButtonClick} />
                         </div>
                         <div>
-                            <input type="button" value="7" onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="8" onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="9" onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="*" onClick={(e) => setValue(value + e.target.value)} />
+                            <input type="button" value="7" onClick={handleButtonClick} />
+                            <input type="button" value="8" onClick={handleButtonClick} />
+                            <input type="button" value="9" onClick={handleButtonClick} />
+                            <input type="button" value="*" onClick={handleButtonClick} />
                         </div>
                         <div>
-                            <input type="button" value="4" onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="5" onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="6" onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="-" onClick={(e) => setValue(value + e.target.value)} />
+                            <input type="button" value="4" onClick={handleButtonClick} />
+                            <input type="button" value="5" onClick={handleButtonClick} />
+                            <input type="button" value="6" onClick={handleButtonClick} />
+                            <input type="button" value="-" onClick={handleButtonClick} />
                         </div>
                         <div>
-                            <input type="button" value="1" onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="2" onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="3" onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="+" onClick={(e) => setValue(value + e.target.value)} />
+                            <input type="button" value="1" onClick={handleButtonClick} />
+                            <input type="button" value="2" onClick={handleButtonClick} />
+                            <input type="button" value="3" onClick={handleButtonClick} />
+                            <input type="button" value="+" onClick={handleButtonClick} />
                         </div>
                         <div>
-                            <input type="button" value="0" onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="." onClick={(e) => setValue(value + e.target.value)} />
-                            <input type="button" value="=" onClick={(e) => setValue(eval(value))} />
+                            <input type="button" value="0" onClick={handleButtonClick} />
+                            <input type="button" value="." onClick={handleButtonClick} />
+                            <input type="button" value="=" onClick={handleEquals} />
                         </div>
                     </form>
                 </div>
@@ -65,5 +100,4 @@ function App() {
         </>
     )
 }
-
 export default App
